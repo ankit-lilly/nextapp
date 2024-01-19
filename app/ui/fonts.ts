b1feca51920dc3013f585340096f47a156012d39
Ankit Bahuguna <bahuguna_ankit@lilly.com>
2024-01-20 00:47:55 +0530

    Missing Font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,12 @@
-import { Inter, Overpass_Mono } from 'next/font/google';
+import { Inter, Overpass_Mono, Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
 export const overPass = Overpass_Mono({
   subsets: ['latin'],
   weight: ['400', '500'],
+});
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
 });
